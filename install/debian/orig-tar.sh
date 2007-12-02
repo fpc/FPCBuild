9f81acb2c16a4d04f96eae72b87625a980c86a30
mazen <mazen@freepascal.org>
2007-12-02 23:48:00 +0000

    Merged revisions 430-431,434-435,438,440 via svnmerge from svn+ssh://mazen@svn.freepascal.org/FPC/svn/fpcbuild/trunk
    ........
      r430 | mazen | 2007-11-18 22:55:36 +0100 (dim, 18 nov 2007) | 2 lines
      
      * a man template for fppkg.
    ........
      r431 | mazen | 2007-11-18 22:57:43 +0100 (dim, 18 nov 2007) | 2 lines
      
      * Moved libasync to fcl debian package.
    ........
      r440 | peter | 2007-12-01 18:15:54 +0100 (sam, 01 déc 2007) | 3 lines
      
        * sync with official debian
        * add fcl-process,fcl-json
    ........
    
    git-svn-id: branches/fixes_2_2@441 -

---
+++ install/debian/orig-tar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+# called by uscan with '--upstream-version' <version> <file>
+TAR=$3
+DIR=fpcbuild-$2
+
+# clean up the upstream tarball
+tar -x -z -f $TAR
+tar -c -z -f $TAR --exclude '*.dll' --exclude '*.exe' --exclude '*.log' $DIR
+rm -rf $DIR
+
+# move to directory 'tarballs'
+if [ -r .svn/deb-layout ]; then
+  . .svn/deb-layout
+  mv $TAR $origDir
+  echo "moved $TAR to $origDir"
+fi
